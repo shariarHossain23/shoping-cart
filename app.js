01f637ce18d9d0821a766232684c24192e7f71f5
shariarhossain23e <shariarhossain23@gamil.comm>
2022-02-13 15:23:48 +0600

    update case total

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('I am in a separate file')
+function updateProduct(product,issTrue){
+    const productInput = document.getElementById(product +"-number");
+    let productNumber = productInput.value;
+    if(issTrue == true){
+        productNumber = parseInt(productNumber) +1;
+        console.log(productNumber)
+    }
+    else if(productNumber > 0){
+        productNumber = parseInt(productNumber) -1;
+    }
+    productInput.value = productNumber;
+    const total = productNumber * 59;
+
+    // show html
+    document.getElementById(product+"-total").innerText = total
+}
+// phone price
+
+//  case price 
+document.getElementById("case-increase").addEventListener("click",function(){
+    updateProduct("case",true)
+})
+document.getElementById("case-decrease").addEventListener("click",function(){
+    updateProduct("case",false)
+})
